51f9cd16832963cd86a890b283d64547e88aac06
WuX <wux@thenest.dev>
2025-08-16 17:26:03 +0300

    feat: add option to disable shadows for panels

--- lua/zenui/components/panel.lua
+++ lua/zenui/components/panel.lua
@@ -6,14 +6,17 @@
 local PANEL = {}
 
 function PANEL:Init()
+    self.Shadow = true
     self:SetColor(ZenUI.Colors.Primary)
 end
 
 function PANEL:Paint(w, h)
-    BSHADOWS.BeginShadow()
-        local x, y = self:GetPos() -- Shadow Fucks With Pos
-        draw.RoundedBox(8, x, y, w, h, self.Color)
-    BSHADOWS.EndShadow(1, 2, 2, 255, 0, 0, true)
+    if self.Shadow then
+        local x, y = self:LocalToScreen(0, 0) -- Shadow Fucks With Pos
+        BSHADOWS.BeginShadow()
+            draw.RoundedBox(4, x, y, w, h, self.Color)
+        BSHADOWS.EndShadow(1, 2, 2, 255, 0, 0, true)
+    end
 
     draw.RoundedBox(4, 0, 0, w, h, self.Color)
 end
@@ -24,6 +27,10 @@ end
 
 function PANEL:GetColor()
     return color
+end
+
+function PANEL:SetShadow(enabled)
+    self.Shadow = enabled
 end
 
 vgui.Register("ZenUI.Panel", PANEL, "EditablePanel")
