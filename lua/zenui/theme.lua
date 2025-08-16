61d8bd0366cfde93854ae77b4791202505fd6485
WuX <wux@thenest.dev>
2025-08-16 17:26:11 +0300

    feat: add tertiary color to theme

--- lua/zenui/theme.lua
+++ lua/zenui/theme.lua
@@ -8,6 +8,7 @@ ZenUI.Themes = ZenUI.Themes or {}
 ZenUI.Themes["Dark"] = {
     Primary = Color(28, 28, 30),
     Secondary = Color(44, 44, 46),
+    Tertiary = Color(56, 56, 58),
     Text = Color(250, 250, 250),
     SecondaryText = Color(175, 175, 175),
     Accent = Color(147, 36, 252)
